96fd92c9a30d41a6bcfd837f44f63806b824f4b7
Rui Castro <rui.castro@gmail.com>
2015-11-08 03:19:32 +0000

    Updated build script

--- build.sh
+++ build.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-dest=`grep ".*id=" addon.xml | cut -f2 -d'"'`
+addon=`grep ".*id=" addon.xml | cut -f2 -d'"'`
+dest=build/$addon
 version=`grep "^\s\+version" addon.xml | cut -f2 -d'"'`
 
 if [ -d $dest ]; then
     rm -r $dest
 fi
+mkdir -p $dest
 
-mkdir $dest
 cp addon.xml $dest/
-cp changelog.txt $dest/
 cp icon.png $dest/
-cp LICENSE.txt $dest/
 cp README.md $dest/
+cp LICENSE.txt $dest/
+cp changelog.txt $dest/
 cp -r lib $dest/
 
 if [ -f $dest-$version.zip ]; then
     rm $dest-$version.zip
 fi
 
-zip -r $dest-$version.zip $dest
-rm -r $dest
+cd build
+zip -r $addon-$version.zip $addon
+mv $addon-$version.zip $addon
+
+rm $addon/README.md
+rm $addon/LICENSE.txt
+rm $addon/changelog.txt
+rm -r $addon/lib
